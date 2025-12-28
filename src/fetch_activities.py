cff5a13d2d88e40dfb87c89c2750f327525b0004
Michal_laptop <michal200323@gmail.com>
2025-12-28 19:52:52 +0100

    fetch_activities update

--- src/fetch_activities.py
+++ src/fetch_activities.py
@@ -3,6 +3,8 @@ import requests
 from datetime import datetime
 import os
 import json
+import pandas as pd
+from pathlib import Path
 
 def fetch_activities_from_strava(access_token, per_page=100, page=1):
     """Sends request to Strava Api and returns data"""
@@ -31,16 +33,39 @@ def fetch_activities_from_strava(access_token, per_page=100, page=1):
         json.dump(activities, f, indent=4)
 
     print(f"Fetched{len(activities)} activities and saved to {file_path}")
+    return file_path
+
+
+def take_important_data(file_path):
+    "Takes important data from json and saves it to /processed as csv"
+    df = pd.read_json(file_path)
+    columns = ["elapsed_time", "distance" ,"total_elevation_gain", "id", "average_speed", "average_cadence", "has_heartrate", "average_heartrate", "max_heartrate"]
+
+    newdf = df[columns].copy()
+
+    newdf["avg_cadence_real"] = newdf["average_cadence"] * 2
+    newdf["elapsed_time_sec"] = newdf["elapsed_time"]
+    newdf["elapsed_time_min"] = newdf["elapsed_time_sec"] / 60
+    newdf["has_cadence"] = newdf["average_cadence"].notna().astype(int)
+    newdf["has_heartrate"] = newdf["has_heartrate"].astype(int)
+    newdf["distance_km"] = newdf["distance"] / 1000
+    newdf["distance_m"] = newdf["distance"]
+    newdf = newdf.drop(["elapsed_time", "distance", "average_cadence"], axis=1)
+
+    newdf = newdf[["id", "elapsed_time_sec", "elapsed_time_min", "distance_m", "distance_km", "has_heartrate", "average_heartrate", "max_heartrate", "has_cadence", "avg_cadence_real", "average_speed", "total_elevation_gain"]]
+
+    name = Path(file_path).stem
+
+    output_dir = os.path.join("data", "processed")
+    os.makedirs(output_dir, exist_ok=True)
+
+    newdf.to_csv(os.path.join(output_dir, f"{name}.csv"), index=False)
+
+
 
 if __name__ == "__main__":
     access_token = get_valid_acces_token()
-    fetch_activities_from_strava(access_token)
+    file_path = fetch_activities_from_strava(access_token)
+    take_important_data(file_path)
 
-def save_raw_json(activities, raw_dir):
-    pass
 
-def update_personal_csv(activities, csv_path):
-    pass
-
-def main(fetch_csv=True):
-    pass
