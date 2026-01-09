36c3ac5110b3e224032b3532994a6074e6ece2a0
Michal_laptop <michal200323@gmail.com>
2026-01-09 13:45:46 +0100

    add 2 tabs

--- dashboard/main.py
+++ dashboard/main.py
@@ -2,16 +2,26 @@ import streamlit as st
 from pathlib import Path
 import pandas as pd
 import sys
-import plotly.graph_objects as go
-
+from streamlit_folium import st_folium
 root_path = Path(__file__).resolve().parent.parent
 sys.path.append(str(root_path))
+
 from src.plots import (
     draw_ridge_plot,
     pace_zones_distribution,
     draw_pace_over_time,
     distance_distribution,
     draw_github_calendar
+)
+
+from src.riegel import riegel, riegel_choose_activity
+from src.scripts import format_time, clean_nan
+from src.map import f_map
+
+st.set_page_config(
+    page_title="Runcast Dashboard",
+    layout="wide", 
+    initial_sidebar_state="expanded"
 )
 
 
@@ -23,98 +33,202 @@ def read_data():
     latest = max(folder.glob("*.csv"), key=lambda p: p.stem)
     df = pd.read_csv(str(latest)).copy()
 
-    st.dataframe(df)
-
     return df
+
 
 def summary(df):
     total_distance = df["distance_km"].sum()
     total_runs = df["id"].count()
     total_time = df["elapsed_time_min"].sum()
-    # avg_pace = df["pace_min/km"].avg()
+    total_time = format_time(total_time)
+
+    avg_pace = df["pace_min/km"].mean()
+    avg_pace = format_time(avg_pace)
 
     #best 5km
-    filtered_df_5km = df[df['distance_km'] < 5.15]
-    fastest_row_5km = filtered_df_5km.sort_values(by='elapsed_time_min').iloc[0]
-
-    pace_5km = fastest_row_5km['pace_min/km']
-    time_5km = fastest_row_5km['elapsed_time_min']
-
+    filtered_df_5km = df[(df['distance_km'] >= 4.8) & (df['distance_km'] < 5.5)]
+    if not filtered_df_5km.empty:
+        fastest_row_5km = filtered_df_5km.sort_values(by='elapsed_time_min').iloc[0]
+    else:
+        pace_5km, time_5km = "-", "-"
+    print(fastest_row_5km)
+    pace_5km = fastest_row_5km['pace_readable']
+    time_5km = fastest_row_5km['time_readable']
 
     #best 10km
 
-    filtered_df_10km = df[df['distance_km'] < 10.15]
-    fastest_row_10km = filtered_df_10km.sort_values(by='elapsed_time_min').iloc[0]
+    filtered_df_10km = df[(df['distance_km'] >= 9.8) & (df['distance_km'] < 11)]
+    if not filtered_df_10km.empty:
+        fastest_row_10km = filtered_df_10km.sort_values(by='elapsed_time_min').iloc[0]
+    else:
+        pace_10km, time_10km = "-", "-"
 
-    pace_10km = fastest_row_10km['pace_min/km']
-    time_10km = fastest_row_10km['elapsed_time_min']
-
-
-    stats = [
-        {"label": "Pace_5km", "pace": f"{pace_5km}km", "Time_5km": f"{time_5km}min"} ,
-        {"label": "Pace_10km", "value": f"{pace_5km}km"} ,
-        {"label": "Pace_5km", "value": f"{pace_5km}km"} 
-    ]
-    
-
-    col_total_time, col_total_min = st.columns(2)
-
-    # with col_total_time:
-    #     tile = st.container(height=80)
-    #     tile.write = ("xdd")
+    pace_10km = fastest_row_10km["pace_readable"]
+    time_10km = fastest_row_10km["time_readable"]
 
 
-def riegel(t1, d1, d2):
-    if d1 == 0:
-        raise ValueError("d1 must be > 0")
-    
-    t2 = t1 * (d2 / d1) ** 1.06
-    st.write(f"Your time will be {t2}")
-    return 
 
-def riegel_choose_activity(df):
-    options = df["id"].tolist()
+    with st.container(border=False):
+        col1, col2, col3, col4 = st.columns(4, border=True)
+        col5, col6 = st.columns(2, border=True)
 
-    selected_id = st.selectbox(
-        "Wybierz aktywność",
-        options
+        with col1:
+            st.metric("Total distance", f"{round((total_distance), 2)}km")
+
+        with col2:
+            st.metric("Total runs", total_runs)
+        
+        with col3:
+            st.metric("Total time", f"{total_time}min")
+
+        with col4:
+            st.metric("Average pace", f"{avg_pace}min/km")
+
+
+        with col5:
+            st.subheader("Best 5km")
+            st.metric("Pace", f"{pace_5km}min/km")
+            st.metric("Time", f"{time_5km}min")
+
+        
+        with col6:
+            st.subheader("Best 10km")
+            st.metric("Pace", f"{pace_10km}min/km")
+            st.metric("Time", f"{time_10km}min")
+
+
+def plots_dashboard(df):
+    with st.container(border=True):
+
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.subheader("Calendar")
+        
+        with col2:
+            df["start_date"] = pd.to_datetime(df["start_date"])
+            options = df["start_date"].dt.year.unique()
+            year = st.selectbox(
+            "Choose year",
+            options)
+
+
+        draw_github_calendar(df, year)
+
+
+    with st.container(border=True):
+
+        col1, col2 = st.columns(2)
+
+        with col1:
+            pace_zones_distribution(df)
+        
+        with col2:
+            distance_distribution(df)
+
+    with st.container(border=True):
+        draw_pace_over_time(df)
+
+    with st.container(border=True):
+            draw_ridge_plot(df)
+
+def show_data(df):
+    columns = ["id","date", "distance_km", "time_readable", "average_heartrate", "has_map"]
+    st.dataframe(df[columns], hide_index=True)
+
+def choose_run(df):
+    options = df["id"]
+    id = st.selectbox(
+        "Choose run (id)",
+        options 
     )
-    selected_row = df[df["id"] == selected_id].iloc[0]
+    return id
 
-    return float(selected_row["distance_km"]), float(selected_row["elapsed_time_min"])
+def run_map(df, id):
+
+    m = f_map(df, id)
+
+    if m is not None:
+        st_folium(m,width="stretch" ,height=500)
+    
+def run_stats(df, id):
+    selected_run = df[df['id'] == id].iloc[0]
+
+    #date, distance, time, heart_rate, max_hr, cadence, pace, elevation_gain 
+
+    raw_delta = ((df["pace_min/km"].mean() - selected_run["pace_min/km"]) / df["pace_min/km"].mean()) * 100
 
 
+    date = clean_nan(selected_run["date"])
+    distance = clean_nan(selected_run["distance_km"], 2)
+    time = clean_nan(selected_run["time_readable"])
+    hr = clean_nan(selected_run["average_heartrate"])
+    max_hr = clean_nan(selected_run["max_heartrate"])
+    cadence = clean_nan(selected_run["avg_cadence_real"])
+    pace = clean_nan(selected_run["pace_readable"])
+    speed = clean_nan(selected_run["average_speed_km/h"], 2)
+    elev = clean_nan(selected_run["total_elevation_gain"])
+
+    delta_pace = clean_nan(raw_delta, 1)
+
+    st.subheader(date)
+
+    col1, col2 ,col3, col4 = st.columns(4, border=True)
+    col5, col6, col7, col8 = st.columns(4, border=True)
+    with col1:
+        st.metric("Distance", f"{distance}km")
+
+    with col2:
+        st.metric("Time", f"{time}min")
+    
+    with col3:
+        st.metric("Pace", f"{pace}min/km", f"{delta_pace:.1f}% vs Average" )
+
+    with col4:
+        st.metric("Speed", f"{speed}km/h")
+
+    with col5:
+        st.metric("Average Heart Rate", f"{hr}bpm")
+
+    with col6:
+        st.metric("Max Heart Reate", f"{max_hr}min")
+    
+    with col7:
+        st.metric("Cadence", f"{cadence}spm")
+
+    with col8:
+        st.metric("Total elevation gain", f"{elev}m")
 
 
+        
 
-
-
-
-
-
-
-
-
-
-st.title("RUNCAST")
-st.write("Input your distance")
 
 
 
 
 if __name__ == "__main__":
     df = read_data()
-    distance, time = riegel_choose_activity(df)
+    st.title("RUNCAST")
+    tab1, tab2, tab3 = st.tabs(["Summary", "Run", "Prediction"])
+    with tab1:
+        summary(df)
+        plots_dashboard(df)
 
-    d2 = st.text_input("Distance:")
+    with tab2:
+        show_data(df)
+        id = choose_run(df)
+        run_stats(df, id)
+        run_map(df, id)
 
-    if d2:
-        d2 = float(d2)
-        t2 = riegel(time, distance, d2)
+    with tab3:
+        d1, t1 = riegel_choose_activity(df)
+        d2 = 5
+        t2 = riegel(t1, d1, d2)
 
-    pace_zones_distribution(df)
-    draw_ridge_plot(df)
-    distance_distribution(df)
-    draw_pace_over_time(df)
-    draw_github_calendar(df)
-    summary(df)
+        # distance, time = riegel_choose_activity(df)
+
+    # d2 = st.text_input("Distance:")
+
+    # if d2:
+    #     d2 = float(d2)
+    #     t2 = riegel(time, distance, d2)
