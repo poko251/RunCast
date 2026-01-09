ded67990cbe9a160cae3f6396d6d879e87139d65
Michal_laptop <michal200323@gmail.com>
2026-01-09 13:48:56 +0100

    add dynamic map updates by ID

--- src/map.py
+++ src/map.py
@@ -2,51 +2,54 @@ import folium
 import pandas as pd
 import ast
 import polyline
+import streamlit as st
 
 
 
-df = pd.read_csv("data/personal/processed/2026-01-08.csv")
+def f_map(df, id):
+    selected_run = df[df['id'] == id].iloc[0]
 
-map_df = df[["start_latlng", "end_latlng", "polyline"]].dropna()
+    if selected_run["has_map"] == 0 :
+        st.warning("This run has no GPS data.")
+        return None
+    else: 
+        st.subheader("Map")
+        start_coords = selected_run['start_latlng']
+        start_l = ast.literal_eval(start_coords)
+        start_l = tuple(start_l)
 
-start_location = map_df["start_latlng"].iloc[2]
-start_l = ast.literal_eval(start_location)
-start_l = tuple(start_l)
+        end_coords = selected_run['end_latlng']
+        end_l = ast.literal_eval(end_coords)
+        end_l = tuple(end_l)
 
-end_location = map_df["end_latlng"].iloc[2]
-end_l = ast.literal_eval(end_location)
-end_l = tuple(end_l)
+        encoded_polyline = selected_run['polyline']
 
-encoded_polyline= map_df["polyline"].iloc[2]
+        route_coordinates = polyline.decode(encoded_polyline)
 
-route_coordinates = polyline.decode(encoded_polyline)
+        m = folium.Map(location=start_l)
 
+        folium.Marker(
+            location=start_l,
+            tooltip="Start location",
+            popup="Start location",
+            icon=folium.Icon(color="green", icon="play"),
+        ).add_to(m)
 
-
-
-m = folium.Map(location=start_l)
-
-folium.Marker(
-    location=start_l,
-    tooltip="Start location",
-    popup="Start location",
-    icon=folium.Icon(color="green", icon="play"),
-).add_to(m)
-
-folium.Marker(
-    location=end_l,
-    tooltip="End location",
-    popup="End location",
-    icon=folium.Icon(color="red", icon="stop"),
-).add_to(m)
+        folium.Marker(
+            location=end_l,
+            tooltip="End location",
+            popup="End location",
+            icon=folium.Icon(color="red", icon="stop"),
+        ).add_to(m)
 
 
-folium.PolyLine(
-    locations=route_coordinates,
-    color="blue",       
-    weight=5,           
-    opacity=0.8,       
-    tooltip="Trasa biegu"
-).add_to(m)
+        folium.PolyLine(
+            locations=route_coordinates,
+            color="blue",       
+            weight=5,           
+            opacity=0.8,       
+            tooltip="Run route"
+        ).add_to(m)
 
-m.save("index.html")
+        m.fit_bounds(route_coordinates)
+        return m
