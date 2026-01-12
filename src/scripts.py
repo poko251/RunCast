a3c2898714326c8e9b13dd9c9c2667f3beca796c
Michal_laptop <michal200323@gmail.com>
2026-01-12 12:22:17 +0100

    add some scripts

--- src/scripts.py
+++ src/scripts.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def format_time(pace_decimal):
+    """Change time format from 6.5 to 6:30"""
     if pd.isna(pace_decimal) or pace_decimal == 0:
         return "-"
     minutes = int(pace_decimal)
@@ -13,6 +14,7 @@ def format_time(pace_decimal):
     return f"{minutes}:{seconds:02d}"
 
 def clean_nan(val, decimals=None):
+    """If a valvue is none return '-' , helps to visualize data in dashboard"""
     if pd.isna(val):
         return "-"
     if decimals is not None:
